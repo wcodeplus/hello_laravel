c485dc1555afc43a1c23b1b0a6e373c688d8f8b0
wcodeplus <wcodeplus@163.com>
2019-04-26 18:37:31 +0800

    mix控制css缓存

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,11 +2,11 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 20px;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
