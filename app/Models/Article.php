73671150c00eadc40cc92bad9fec6e2307e98d72
wcodeplus <wcodeplus@163.com>
2019-04-27 17:09:54 +0800

    实现用户显示

---
+++ app/Models/Article.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    // Article 默认对应表 articles
+    // protected $table = "my_articles";
+
+}
